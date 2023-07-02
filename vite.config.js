84da0074934666413335532f37b97dd9a059fc62
dprice17 <109916870+dprice17@users.noreply.github.com>
2023-07-02 11:08:03 -0400

    code upate

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,93 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import fs from "fs/promises";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: "/bestbank",
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: "./src/index.js",
+      external: ["uuid"],
+    },
+  },
+  esbuild: {
+    loader: "jsx",
+    include: /src\/.*\.jsx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        {
+          name: "load-js-files-as-jsx",
+          setup(build) {
+            build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => ({
+              loader: "jsx",
+              contents: await fs.readFile(args.path, "utf8"),
+            }));
+          },
+        },
+      ],
+    },
+  },
 });
+
+/*import fs from "fs/promises";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig(() => ({
+  esbuild: {
+    loader: "jsx",
+    include: /src\/.*\.jsx?$/,
+    // loader: "tsx",
+    // include: /src\/.*\.[tj]sx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        {
+          name: "load-js-files-as-jsx",
+          setup(build) {
+            build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => ({
+              loader: "jsx",
+              contents: await fs.readFile(args.path, "utf8"),
+            }));
+          },
+        },
+      ],
+    },
+  },
+}));
+
+/*
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import babel from "@rollup/plugin-babel";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    babel({
+      babelHelpers: "bundled",
+      loader: { ".js": "jsx" },
+      presets: [
+        [
+          "@babel/preset-react",
+          {
+            runtime: "automatic",
+          },
+        ],
+      ],
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      input: "src/index.js",
+      external: ["uuid"],
+    },
+  },
+});
+*/
